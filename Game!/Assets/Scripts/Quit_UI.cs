835df3fc5f7b06a3a9b7396480e69baaf76948cb
Nathalia McMillan <nam1565@g.rit.edu>
2026-04-22 13:50:51 -0400

    stuff

---
+++ Game!/Assets/Scripts/Quit_UI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Quits the player when the user hits escape
+
+public class Quit_UI : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("delete"))
+        {
+            Application.Quit();
+        }
+    }
+}
